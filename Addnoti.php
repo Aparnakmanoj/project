3bcaca265a825792f3fc3b84244aeeb1e65ef399
Aparnakmanoj <95676490+Aparnakmanoj@users.noreply.github.com>
2022-11-22 23:18:19 +0530

    Add files via upload

---
+++ Addnoti.php
@@ -0,0 +1,380 @@
+<?php
+  include "../connect.php";
+
+ // $getid=$_SESSION['uname'];
+ $getid=$_SESSION['username'];
+ 
+if($getid!=0)
+{
+  
+ 
+   if(isset($_POST['Submit']))
+{   
+      
+  
+ //session_start();
+ //$cid=$_SESSION['cid'];
+	$subject=$_POST['t1'];
+	$msg=$_POST['t2'];
+	$ndate=date("Y-m-d");
+	$n=mysqli_query($conn,"insert into tbl_notification(cid,sub,msg,ndate) values('18','$subject','$msg','$ndate')");
+	
+	?>
+	<script>alert("New notification added")</script>
+	<?php
+}
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ARMPMS</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    
+    </ul>
+
+
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Messages Dropdown Menu -->
+      
+      <!-- Notifications Dropdown Menu -->
+
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+     
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="" class="brand-link">
+      <img src="../images/logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">ARMPMS</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+   <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block" style="margin-top: -12px">HOD</a>
+         <a href="#" style="color: #239db1; font-size: 15px"><i class="fa fa-circle text-primary" style="font-size: 13px;"></i>ADMIN</a>
+        </div>
+
+      </div>
+
+      <!-- Sidebar Menu -->
+       <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item menu-open">
+            <a href="dashboard.php" class="nav-link ">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+              
+          </li>
+        
+          <li class="nav-item">
+            <a href="faculty.php" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                FACULTY LIST
+                <i class="fas fa-angle-left right"></i>
+                <span class="badge badge-info right"></span>
+              </p>
+            </a>
+            </li>
+            
+              <li class="nav-item">
+                <a href="approveview.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>STUDENT APPROVAL</p>
+                </a>
+              </li>
+            
+            <li class="nav-item">
+                <a href="batchview.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>BATCH VIEW</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="teachreg.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>FACULTY REGISTRATION</p>
+                </a>
+              </li>
+            
+            <li class="nav-item">
+                <a href="currentco.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>CURRENT COORDINATOR</p>
+                </a>
+              </li>
+             <li class="nav-item">
+                <a href="coassign.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>COORDINATOR ASSIGNMENT</p>
+                </a>
+              </li>
+        <li class="nav-item">
+                <a href="Addnoti.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p> ADD NOTIFICATIONS</p>
+                </a>
+              </li>
+            
+            
+              <li class="nav-item">
+                <a href="notiview.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>NOTIFICATION</p>
+                </a>
+              </li>
+        
+          <li class="nav-item">
+            <a href="profile.php" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                PROFILE
+          
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../logout.php" class="nav-link">
+              <i class="nav-icon fas fa-power-off"></i>
+              <p>
+                LOGOUT
+              </p>
+            </a>
+          </li>
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Notifications</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Noficatons</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          
+       
+
+            <div class="card">
+              <div class="card-header">
+                 
+                
+                            
+              <!-- /.card-header -->
+                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<body>
+<form name="form1" method="post" action="">
+  <div class="modal-content">
+                            <div class="modal-header">
+                              
+                             
+                               
+   <center>
+      <p>&nbsp;</p>
+        <div class="card card-primary">
+                              <div class="card-body">
+                                <div class="row">
+                                   
+                                  <div class="form-group">
+      
+      <table width="54%" border="0">
+        <tr> 
+            <h4 class="modal-title">Add Notifications</h4>
+          <td >Subject</td><td height="28"><input  name="t1" type="text" id="t1" size="50" class="form-control"></td>
+        </tr>
+        
+		<tr> 
+          <td>Message</td><td height="28"><textarea name="t2" type="text" id="t2" size="50" class="form-control"></textarea></td>
+        </tr>
+          
+                               <div class="form-group">
+                                  
+        <tr> 
+            <center>
+          <td><input type="submit" class="btn btn-primary" name="Submit" value="Submit"></td>
+            </center>
+        </tr>
+      </table>
+	
+    <div align="justify"></div>
+  </div>
+                                      
+          <footer class="main-footer">
+    <strong>Footer <a href="">Project Management System</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Footer</b>
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="plugins/jszip/jszip.min.js"></script>
+<script src="plugins/pdfmake/pdfmake.min.js"></script>
+<script src="plugins/pdfmake/vfs_fonts.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- Page specific script -->
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": [""]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+</body>
+</html>                            
+        <?php
+}
+else
+{
+    header("Location:login.php");    
+}
+               
+               
+               ?>                               
+                                      
+                                      
+                       
